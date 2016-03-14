1b363da118eb42d677627e1957c84efbd0669365
Oremade, Oluwatobi Oluwatomisin <ooo130330@sairlabmac005.local>
2016-03-14 18:10:27 -0500

    Initial Commit

---
+++ Simple Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Weather
+//
+//  Created by Oremade, Oluwatobi Oluwatomisin on 3/14/16.
+//  Copyright © 2016 Oremade, Oluwatobi Oluwatomisin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Simple WeatherTests/Simple_WeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_WeatherTests.swift
+//  Simple WeatherTests
+//
+//  Created by Oremade, Oluwatobi Oluwatomisin on 3/14/16.
+//  Copyright © 2016 Oremade, Oluwatobi Oluwatomisin. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Weather
+
+class Simple_WeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
